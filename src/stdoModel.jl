7e86a38fde517c13991db2402a21c302ae10f7c1
Rodrigo Benoliel <rodrigobenoliel@psr-inc.com>
2025-05-01 11:33:05 -0300

    Adds hour and scenarios to dimensions

--- src/stdoModel.jl
+++ src/stdoModel.jl
@@ -1,9 +1,9 @@
 function StdoBuildVariables!(m, study::StdoStudy)
-    @variable(m, 0 <= losses[1:study.circuits.size])
-    @variable(m, flow[icir=1:study.circuits.size])
-    @variable(m, 0 <= powerSupply[1:study.buses.size])
-    @variable(m, 0 <= powerConsumption[1:study.buses.size])
-    @variable(m, 0 <= deficit[1:study.buses.size])
+    @variable(m, 0 <= losses[1:study.circuits.size,1:study.hours])
+    @variable(m, flow[icir=1:study.circuits.size,1:study.hours])
+    @variable(m, 0 <= powerSupply[1:study.buses.size,1:study.hours])
+    @variable(m, 0 <= powerConsumption[1:study.buses.size,1:study.hours])
+    @variable(m, 0 <= deficit[1:study.buses.size,1:study.hours,1:study.scenarios])
 end
 
 function StdoBuildObjectiveFunction!(m, study::StdoStudy)
@@ -15,8 +15,8 @@ function StdoBuildObjectiveFunction!(m, study::StdoStudy)
 
     penalty_deficit = 1e6 # penalty for deficit
     @objective(m, Min, 
-        sum(losses[icircuit] for icircuit in 1:study.circuits.size) + 
-        sum(penalty_deficit * deficit[ibus] for ibus in 1:study.buses.size)
+        sum(losses[icircuit,ihour] for icircuit in 1:study.circuits.size, ihour in 1:study.hours) + 
+        sum(penalty_deficit * deficit[ibus,ihour,iscenario] for ibus in 1:study.buses.size, ihour in 1:study.hours, iscenario in 1:study.scenarios)
     )
 end
 
@@ -28,26 +28,26 @@ function StdoBuildConstraints!(m, study::StdoStudy)
     deficit = m[:deficit]
     losses = m[:losses]    
 
-    @constraint(m, node_balance[ibus=1:study.buses.size],
-        sum(flow[icircuit] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) - 
-        sum(flow[icircuit] for icircuit in 1:study.circuits.size if study.circuits.busFrom[icircuit] == ibus) -
-        sum(losses[icircuit] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) +
-        powerSupply[ibus] - powerConsumption[ibus] == 0
+    @constraint(m, node_balance[ibus=1:study.buses.size, ihour=1:study.hours],
+        sum(flow[icircuit,ihour] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) - 
+        sum(flow[icircuit,ihour] for icircuit in 1:study.circuits.size if study.circuits.busFrom[icircuit] == ibus) -
+        sum(losses[icircuit,ihour] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) +
+        powerSupply[ibus,ihour] - powerConsumption[ibus,ihour] == 0
     )
 
-    @constraint(m, demand_supply[ibus=1:study.circuits.size],
-        powerConsumption[ibus] + deficit[ibus] == sum(study.loads.power[iload] for iload in 1:study.loads.size if study.loads.load2bus[iload] == ibus)
+    @constraint(m, demand_supply[ibus=1:study.circuits.size, ihour=1:study.hours, iscenario=1:study.scenarios],
+        powerConsumption[ibus,ihour] + deficit[ibus,ihour,iscenario] == sum(study.loads.power[iload] for iload in 1:study.loads.size if study.loads.load2bus[iload] == ibus)
     )
 
-    @constraint(m, max_power_supply[ibus=1:study.buses.size],
-        powerSupply[ibus] <= sum(study.generators.power[igenerator] for igenerator in 1:study.generators.size if study.generators.gen2bus[igenerator] == ibus)
+    @constraint(m, max_power_supply[ibus=1:study.buses.size, ihour=1:study.hours],
+        powerSupply[ibus,ihour] <= sum(study.generators.power[igenerator] for igenerator in 1:study.generators.size if study.generators.gen2bus[igenerator] == ibus)
     )
 
-    @constraint(m, circuit_capacity_up[icircuit=1:study.circuits.size],
-        flow[icircuit] <= study.circuits.capacity[icircuit]
+    @constraint(m, circuit_capacity_up[icircuit=1:study.circuits.size, ihour=1:study.hours],
+        flow[icircuit,ihour] <= study.circuits.capacity[icircuit]
     )
-    @constraint(m, circuit_capacity_down[icircuit=1:study.circuits.size],
-        flow[icircuit] >= -study.circuits.capacity[icircuit]
+    @constraint(m, circuit_capacity_down[icircuit=1:study.circuits.size, ihour=1:study.hours],
+        flow[icircuit,ihour] >= -study.circuits.capacity[icircuit]
     )
     
     return m
