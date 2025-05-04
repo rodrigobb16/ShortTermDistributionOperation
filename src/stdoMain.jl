feb4d0a1aadf18695ff60283c679eb070e691fa3
Iury <iurysaboia927@gmail.com>
2025-05-04 20:54:17 -0300

    Model adjustment

--- src/stdoMain.jl
+++ src/stdoMain.jl
@@ -17,34 +17,43 @@ function main(casepath::String)
     println("Base model built successfully!")
     
     println("Solving model...")
-    circuits_beta0 = [Float64[] for _ in 1:study.circuits.size]
-    circuits_beta1 = [Float64[] for _ in 1:study.circuits.size]
+    flow = m[:flow]
+    losses = m[:losses]    
+
     max_iter = 15
-    iter = 0
     calc_losses = zeros(study.circuits.size)
-    while true
-        iter += 1
-        println("Iteration $iter...")
-        write_to_file(m, "stdo.lp")
-        optimize!(m)
-        calc_losses = study.circuits.resistance .* (value.(m[:flow]) .^ 2)
-        if all(abs.(calc_losses .- value.(m[:losses])) .< 1e-5) || iter >= max_iter
-            break
+    for hour in 1:study.hours
+        println("Hour $hour...")
+        circuits_beta0 = [Float64[] for _ in 1:study.circuits.size]
+        circuits_beta1 = [Float64[] for _ in 1:study.circuits.size]
+        iter = 0
+        while true
+            iter += 1
+            println("Iteration $iter...")
+            write_to_file(m, "stdo.lp")
+            optimize!(m)
+            calc_losses = study.circuits.resistance .* (value.(flow[:,hour]) .^ 2)
+            if all(abs.(calc_losses .- value.(losses[:,hour])) .< 1e-5) || iter >= max_iter
+                break
+            end
+            
+            for icircuit in 1:study.circuits.size
+                push!(circuits_beta1[icircuit], 2 * study.circuits.resistance[icircuit] .* value.(flow[icircuit,hour]))
+                push!(circuits_beta0[icircuit], calc_losses[icircuit] .- circuits_beta1[icircuit][end] * value.(flow[icircuit,hour]))
+            end
+            @constraint(m, [icircuit=1:study.circuits.size],
+                losses[icircuit,hour] >= circuits_beta0[icircuit][end] + circuits_beta1[icircuit][end] * flow[icircuit,hour]
+            )
         end
-        
-        for icircuit in 1:study.circuits.size
-            push!(circuits_beta1[icircuit], 2 * study.circuits.resistance[icircuit] .* value.(m[:flow][icircuit]))
-            push!(circuits_beta0[icircuit], calc_losses[icircuit] .- circuits_beta1[icircuit][end] * value.(m[:flow][icircuit]))
-        end
-        @constraint(m, [icircuit=1:study.circuits.size],
-            m[:losses][icircuit] >= circuits_beta0[icircuit][end] + circuits_beta1[icircuit][end] * m[:flow][icircuit]
-        )
+
+        println("Iteration count: ", iter)
+        println("Losses linearization gap: ", maximum(abs.(calc_losses .- value.(losses[:,hour]))))
     end
     
     println("Model solved successfully!")
     println("Objective value: ", objective_value(m))
-    println("Iteration count: ", iter)
-    println("Losses linearization gap: ", maximum(abs.(calc_losses .- value.(m[:losses]))))
+    
+    
 
     println("STDO SUCCESS!")
 
