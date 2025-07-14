560cea04899473ed128fa0eaaee13951064b26e3
Iury <iurysaboia927@gmail.com>
2025-07-14 10:15:36 -0300

    Model adjustment

--- src/stdoModel.jl
+++ src/stdoModel.jl
@@ -49,19 +49,20 @@ function StdoBuildConstraints!(m, study::StdoStudy)
     batStorage = m[:batStorage]
     x = m[:x]
 
-    @constraint(m, node_balance[ibus=1:study.buses.size, ihour=1:study.hours, iscenario=1:study.scenarios],
-        sum(flow[icircuit,ihour,iscenario] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) - 
-        sum(flow[icircuit,ihour,iscenario] for icircuit in 1:study.circuits.size if study.circuits.busFrom[icircuit] == ibus) -
-        sum(losses[icircuit,ihour,iscenario] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) +
-        powerSupply[ibus,ihour] + powerSupply2Nivel[ibus,ihour,iscenario] - powerConsumption[ibus,ihour,iscenario] == 0
-    )
-
-    # @constraint(m, demand_supply[ibus=1:study.buses.size, ihour=1:study.hours, iscenario=1:study.scenarios],
-    #     powerConsumption[ibus,ihour,iscenario] + deficit[ibus,ihour,iscenario] == 
-    #     sum(study.loads.power[iload] for iload in 1:study.loads.size if study.loads.load2bus[iload] == ibus) - 
-    #     sum(study.renewables.power[igenerator][iscenario][ihour] for igenerator in 1:study.renewables.size if study.renewables.gen2bus[igenerator] == ibus) -
-    #     sum(powerBat[ibattery, ihour, iscenario] for ibattery in 1:study.batteries.size if study.batteries.battery2bus[ibattery] == ibus)
+    ## Sem bateria
+    # @constraint(m, node_balance[ibus=1:study.buses.size, ihour=1:study.hours, iscenario=1:study.scenarios],
+    #     sum(flow[icircuit,ihour,iscenario] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) - 
+    #     sum(flow[icircuit,ihour,iscenario] for icircuit in 1:study.circuits.size if study.circuits.busFrom[icircuit] == ibus) -
+    #     sum(losses[icircuit,ihour,iscenario] for icircuit in 1:study.circuits.size if study.circuits.busTo[icircuit] == ibus) +
+    #     powerSupply[ibus,ihour] + powerSupply2Nivel[ibus,ihour,iscenario] - powerConsumption[ibus,ihour,iscenario] == 0
     # )
+
+    @constraint(m, demand_supply[ibus=1:study.buses.size, ihour=1:study.hours, iscenario=1:study.scenarios],
+        powerConsumption[ibus,ihour,iscenario] + deficit[ibus,ihour,iscenario] == 
+        sum(study.loads.power[iload] for iload in 1:study.loads.size if study.loads.load2bus[iload] == ibus) - 
+        sum(study.renewables.power[igenerator][iscenario][ihour] for igenerator in 1:study.renewables.size if study.renewables.gen2bus[igenerator] == ibus) -
+        sum(powerBat[ibattery, ihour, iscenario] for ibattery in 1:study.batteries.size if study.batteries.battery2bus[ibattery] == ibus)
+    )
 
     # Sensibilidade sem bateria
     @constraint(m, demand_supply[ibus=1:study.buses.size, ihour=1:study.hours, iscenario=1:study.scenarios],
@@ -85,7 +86,7 @@ function StdoBuildConstraints!(m, study::StdoStudy)
         end
     end
 
-    # # Sensibilidade sem retriçoes de capacidade de circuitos
+    # # Sensibilidade sem restrições de capacidade de circuitos
     # for icircuit in 1:study.circuits.size
     #     if study.circuits.type[icircuit] != "switch"
     #         # @constraint(m, flow[icircuit,1:study.hours,1:study.scenarios] .<= study.circuits.capacity[icircuit])
@@ -103,16 +104,16 @@ function StdoBuildConstraints!(m, study::StdoStudy)
         study.buses.size - 1 - sum(study.circuits.type[icircuit] != "switch" for icircuit in 1:study.circuits.size)
     )
 
-    # # Battery storage constraints
-    # @constraint(m, [ibattery=1:study.batteries.size, ihour=1:study.hours, iscenario=1:study.scenarios],
-    #     batStorage[ibattery, ihour, iscenario] <= study.batteries.capacity[ibattery]
-    # )
-    # @constraint(m, [ibattery=1:study.batteries.size, ihour=1, iscenario=1:study.scenarios],
-    #     batStorage[ibattery, ihour, iscenario] == study.batteries.initial_charge[ibattery] - powerBat[ibattery, ihour, iscenario]
-    # )
-    # @constraint(m, [ibattery=1:study.batteries.size, ihour=2:study.hours, iscenario=1:study.scenarios],
-    #     batStorage[ibattery, ihour, iscenario] == batStorage[ibattery, ihour-1, iscenario] - powerBat[ibattery, ihour, iscenario]
-    # )
+    # Battery storage constraints
+    @constraint(m, [ibattery=1:study.batteries.size, ihour=1:study.hours, iscenario=1:study.scenarios],
+        batStorage[ibattery, ihour, iscenario] <= study.batteries.capacity[ibattery]
+    )
+    @constraint(m, [ibattery=1:study.batteries.size, ihour=1, iscenario=1:study.scenarios],
+        batStorage[ibattery, ihour, iscenario] == study.batteries.initial_charge[ibattery] - powerBat[ibattery, ihour, iscenario]
+    )
+    @constraint(m, [ibattery=1:study.batteries.size, ihour=2:study.hours, iscenario=1:study.scenarios],
+        batStorage[ibattery, ihour, iscenario] == batStorage[ibattery, ihour-1, iscenario] - powerBat[ibattery, ihour, iscenario]
+    )
 
     return m
 end
